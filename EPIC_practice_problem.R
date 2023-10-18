8a2982ee4f2f403e23fdf6962f9a9c46779ce22a
Kate Johnson <kate.johnson@ubc.ca>
2023-10-18 16:38:08 -0700

    Creating toy events matrix

---
+++ EPIC_practice_problem.R
@@ -0,0 +1,12 @@
+library(epicR)
+settings <- get_default_settings()
+settings$record_mode <-2
+settings$n_base_agents <- 9e3
+
+init_session(settings=settings)
+run()
+events <- as.data.frame(Cget_all_events_matrix())
+
+EPIC_events_matrix <- write.csv2(events, "EPIC_events_matrix.csv", row.names=F)
+
+terminate_session()
